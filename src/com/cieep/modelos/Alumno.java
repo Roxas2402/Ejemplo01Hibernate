67c823151a2dc052247100882c311c6e3f5709f4
roxas <jeropastrana@gmail.com>
2022-10-24 19:12:16 +0200

    aa

---
+++ src/com/cieep/modelos/Alumno.java
@@ -0,0 +1,46 @@
+package com.cieep.modelos;
+
+public class Alumno {
+    private int id;
+    private String nombre;
+    private String apellidos;
+    private String dni;
+
+    public Alumno(String nombre, String apellidos, String dni) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+}
